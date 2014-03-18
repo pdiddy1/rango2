ef0e51cde1b788c88d6ff587b394b241cf82ea00
pdiddy1 <patrickcdoyle@gmail.com>
2014-03-17 21:00:43 -0400

    about page template added

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,4 +19,8 @@ def index(request):
 	return render_to_response('rango/index.html', context_dict, context)
 	
 def about(request):
-	return HttpResponse("Rango says: Here is the about page. <a href='/rango/'>Index</a>")
+	context = RequestContext(request)
+	context_dict = {'boldmessage': "I am even bolder font from the context.'"}
+	return render_to_response('rango/about.html', context_dict, context)
+	
+	#	return HttpResponse("Rango says: Here is the about page. <a href='/rango/'>Index</a>")
